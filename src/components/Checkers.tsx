dd8f2ab5599d5c4fc4865eb2f6e7926a66d858ee
Jack Ponder <jackponder3@gmail.com>
2022-08-27 22:49:14 -0400

    added king pieces to checkers

--- src/components/Checkers.tsx
+++ src/components/Checkers.tsx
@@ -1,4 +1,5 @@
 import { useState, DragEventHandler } from "react";
+import { TbCrown } from "react-icons/tb";
 
 import "../styles/Board.css";
 
@@ -54,39 +55,40 @@ export default function Checkers() {
   const drop: DragEventHandler = (event) => {
     event.preventDefault();
     const targetSpace = parseInt(event.currentTarget.id);
-    if (turn !== board[currentPiece]) {return}
+    if (turn !== Math.floor(board[currentPiece])) {return}
     if (board[targetSpace]) {return}
 
-    const possibleMoves = turn === 1 ? [
+    const possibleMoves = !Number.isInteger(board[currentPiece]) ? [
+      currentPiece - 7,
+      currentPiece - 9,
+      currentPiece + 7,
+      currentPiece + 9,
+    ] : turn === 1 ? [
       currentPiece - 7,
       currentPiece - 9,
     ] : [
       currentPiece + 7,
       currentPiece + 9,
     ];
-    const possibleJumps = turn === 1 ? [
-      currentPiece - 14,
-      currentPiece - 18,
-    ] : [
-      currentPiece + 14,
-      currentPiece + 18,
-    ];
+    const possibleJumps = possibleMoves.map(value => 
+      currentPiece + 2 * (value - currentPiece)
+    );
 
     if (possibleMoves.includes(targetSpace)) {
-      setBoard(board.map((value, index) => 
-        index === currentPiece ? 
-        0 : index === targetSpace ?
-        turn : value
-      ));
-    } else if (possibleJumps.includes(targetSpace) && 
-      board[findMid(currentPiece, targetSpace)] === (turn === 1 ? 2 : 1)) {
-      setBoard(board.map((value, index) => 
-        index === currentPiece || index === findMid(currentPiece, targetSpace) ? 
-        0 : index === targetSpace ?
-        turn : value
-      ));
+      board[targetSpace] = board[currentPiece];
+      board[currentPiece] = 0;
+    } else if (possibleJumps.includes(targetSpace) && board[findMid(currentPiece, targetSpace)] === (turn === 1 ? 2 : 1)) {
+      board[targetSpace] = board[currentPiece];
+      board[findMid(currentPiece, targetSpace)] = 0;
+      board[currentPiece] = 0;
     } else {
       return;
+    }
+
+    if (turn === 1 && targetSpace < 8) {
+      board[targetSpace] = 1.5;
+    } else if (turn === 2 && targetSpace >= 56) {
+      board[targetSpace] = 2.5;
     }
 
     setWinner(checkWinner());
@@ -107,8 +109,13 @@ export default function Checkers() {
         {board.map((value, index) => 
           <div id={index.toString()} onDrop={drop} onDragOver={allowDrop}>
             {value ? 
+              Number.isInteger(value) ? 
               <div className={`piece player-${value}`} 
-                onDrag={drag} draggable={turn === value ? true : false} >  
+                onDrag={drag} draggable={turn === value ? true : false}>  
+              </div> : 
+              <div className={`piece player-${Math.floor(value)}`}
+                onDrag={drag} draggable={turn === Math.floor(value) ? true : false}>
+                <TbCrown size="50%" />
               </div> :
               null
             }
